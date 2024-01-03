8517bd558d08036b2004e969c455dd1021dafe8a
Xcom <sahandhesar@gmail.com>
2024-01-03 10:57:51 +0100

    BufferUtil class added and removed config

---
+++ src/main/java/util/BufferUtil.java
@@ -0,0 +1,23 @@
+package util;
+
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+public class BufferUtil {
+
+    public static FloatBuffer storeDataInFloatBuffer(float[] data) {
+        FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+        return buffer;
+    }
+
+    public static IntBuffer storeDataInIntBuffer(int[] data) {
+        IntBuffer buffer = BufferUtils.createIntBuffer(data.length);
+        buffer.put(data);
+        buffer.flip();
+        return buffer;
+    }
+}
--- src/main/java/util/Config.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class Config {
-
-    public static final int WIDTH = 1024;
-    public static final int HEIGHT = 720;
-}
